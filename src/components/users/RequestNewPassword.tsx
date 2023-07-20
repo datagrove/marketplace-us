3a8e7ffadb641cd1d871e304176ac25ddc94e52f
Meagan <meagan.h.smith@gmail.com>
2023-07-20 14:09:45 -0500

    Update colors for accessibility contrast

--- src/components/users/RequestNewPassword.tsx
+++ src/components/users/RequestNewPassword.tsx
@@ -29,19 +29,19 @@ export const NewPassword: Component = () => {
 
   return (
     <div>
-      <div class="row flex flex-col">
+      <div class="row flex flex-col bg-background1 dark:bg-background1-DM">
         <div class="col-6 form-widget" aria-live="polite">
           <form class="form-widget" onSubmit={handleReset}>
             <div class="p-2 w-full">
               <label 
                 for="email"
-                class="px-2 text-secondaryText"
+                class="px-2 text-text1 dark:text-text1-DM"
               >
                 Email
               </label>
               <input
                 id="email"
-                class="inputField rounded px-1"
+                class="inputField border border-border dark:border-border-DM rounded px-1"
                 type="email"
                 placeholder="Your email"
                 value={email()}
@@ -51,10 +51,10 @@ export const NewPassword: Component = () => {
             <div class="flex justify-center">
               <button 
                 type="submit" 
-                class="button block" 
+                class="button block border border-border dark:border-border-DM bg-btn1 dark:bg-btn1-DM px-4 rounded-full my-2 hover:bg-btn1hov dark:hover:bg-btn1hov-DM" 
                 aria-live="polite"
               >
-                {loading() ? <span>Loading</span> : <span class="text-text1">Reset</span>}
+                {loading() ? <span>Loading</span> : <span class="text-text2 dark:text-text2-DM">Reset</span>}
               </button>
             </div>
           </form>
