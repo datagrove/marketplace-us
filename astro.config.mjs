8547d5de21b14d488f5690df1f32f409e032fe52
r-southworth <rachael.southworth@datagrove.com>
2023-07-28 11:34:45 -0400

    setup for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,5 +35,14 @@ export default defineConfig({
   trailingSlash: 'never',
   build: {
     format: 'file'
-  }
+  },
+  vite: {
+    define: {
+      'process.env.API_URL': JSON.stringify(process.env.API_URL),
+      'process.env.anon_key': JSON.stringify(process.env.anon_key),
+      'process.env.PUBLIC_STRIPE_API': JSON.stringify(process.env.PUBLIC_STRIPE_API),
+      'process.env.PRIVATE_STRIPE_API': JSON.stringify(process.env.PRIVATE_STRIPE_API),
+      'process.env.PRIVATE_STRIPE_ENDPOINT': JSON.stringify(process.env.PRIVATE_STRIPE_ENDPOINT),
+    }
+  }  
 });
