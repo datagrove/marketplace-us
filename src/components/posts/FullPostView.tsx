ea5cf2a775ef339fa71c55f51b3df87193a764bf
r-southworth <rachael.southworth@datagrove.com>
2023-08-06 15:17:03 -0400

    working on image carousel

--- src/components/posts/FullPostView.tsx
+++ src/components/posts/FullPostView.tsx
@@ -79,39 +79,107 @@ export const ViewFullPost: Component<Props> = (props) => {
 
     const downloadImages = async (image_Urls: string) => {
         try {
-            const { data, error } = await supabase.storage
-                .from("post.image")
-                .createSignedUrls(image_Urls.split(','), 60, { download: true })
-            if (error) {
-                throw error;
-            } else {
-            console.log(data)
-            data.forEach(async (image: any) => {
-                const response = await fetch(image.signedUrl)
-                const blob = await response.blob()
-                const blob_url = URL.createObjectURL(blob)
-                const image_url = blob_url.split('blob:')[1]
-                console.log(image_url.toString())
-                setPostImages([...postImages(), image_url]);
-                console.log(postImages())
+            const imageUrls = image_Urls.split(',');
+            imageUrls.forEach(async (imageUrl: string) => {
+                const { data, error } = await supabase.storage
+                    .from("post.image")
+                    .download(imageUrl);
+                if (error) {
+                    throw error;
+                }
+                const url = URL.createObjectURL(data);
+                setPostImages([...postImages(), url]);
             })
-            }
-            
-            // setPostImages([...postImages(), data[0]]);
         } catch (error) {
             console.log(error)
-        }  
+        }
     }
+
+    // let slideIndex = 1;
+    // showSlide(slideIndex)
+
+    // function moveSlide(n: number) {
+    //     showSlide(slideIndex += n);
+    // }
+
+    // function currentSlide(n: number) {
+    //     showSlide(slideIndex = n);
+    // }
+
+    // function showSlide(n: number) {
+    //     let i;
+    //     const slides = document.getElementsByClassName("slide");
+    //     console.log(slides)
+    //     const dots = document.getElementsByClassName("dot");
+
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].classList.add("hidden");
+    //     }
+
+    //     for (i = 0; i < dots.length; i++) {
+    //         dots[i].classList.remove('active');
+    //     }
+
+    //     //show the active slide
+    //     slides[slideIndex - 1].classList.remove("hidden");
+
+    // }
 
     return (
         <div>
-            <ul>
-                {postImages().map((image: string) => (
-                    <li>
-                        <img src={image} />
-                    </li>
-                ))}
-            </ul>
+            <div class="relative w-full">
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    {postImages().map((image: string, index: number) => (
+                        <div class="slide duration-700 ease-in-out">
+                            <img
+                                src={image}
+                                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                                alt={`Post Image ${index + 1}`} />
+                        </div>
+                    ))}
+
+                </div>
+                <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                    {postImages().map((image: string, index: number) => (
+                        <button 
+                        type="button" 
+                        class="dot w-3 h-3 rounded-full cursor-pointer" 
+                        aria-label={`Slide ${index + 1}`}
+                        onClick={() => currentSlide(index + 1)} 
+                        />
+                    ))}
+                </div>
+                <button
+                    type="button"
+                    class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    onclick={() => moveSlide(-1)}
+                >
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button 
+                type="button" 
+                class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" 
+                onclick={() => moveSlide(1)}>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
             <DeletePostButton Id={+props.id!} UserId={(post()?.user_id !== undefined ? (post()!.user_id) : (""))} />
             <h1 class="text-xl w-full font-bold">{post()?.title}</h1>
             <p>Provider: {post()?.provider_name}</p>
