fc9358535a24252e54c70bee68d381b3425340a1
Meagan <meagan.h.smith@gmail.com>
2023-08-08 16:05:01 -0400

    Add conditional rendering for category icons based on preferred color scheme - not working yet

--- src/components/services/CategoryCarousel.tsx
+++ src/components/services/CategoryCarousel.tsx
@@ -25,6 +25,7 @@ import rightArrow from '../../assets/categoryIcons/circled-right-arrow.svg'
 import leftArrow from '../../assets/categoryIcons/circled-left-arrow.svg'
 import beauty from '../../assets/categoryIcons/beauty-salon.svg'
 import finance from '../../assets/categoryIcons/banking-bank.svg'
+import financeDM from '../../assets/categoryIcons/banking-bank-DM.svg'
 import { currentLanguage } from '../../lib/languageSelectionStore';
 
 let categories: Array<any> = []
@@ -55,6 +56,7 @@ categories.map(
             category.icon = palette
         } else if (category.id === 7) {
             category.icon = finance
+            category.iconDM = financeDM
         } else if (category.id === 8) {
             category.icon = cleaner
         } else if (category.id === 9) {
@@ -90,6 +92,9 @@ interface Props {
 
 // const [selected, setSelected] = createSignal(false);
 
+let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+let light = window.matchMedia("(prefers-color-scheme: light)" || "(prefers-color-scheme: no-preference" ).matches;
+
 export const CategoryCarousel: Component<Props> = (props) => {
 
     return (
@@ -102,7 +107,7 @@ export const CategoryCarousel: Component<Props> = (props) => {
                         />
                     </button>
 
-                    <div class="flex justify-between items-center w-full overflow-auto pb-4">
+                    <div class="flex justify-between items-center w-full overflow-auto py-4">
                         { allCategoryInfo?.map((item) => (
                             
                             <button 
@@ -119,11 +124,35 @@ export const CategoryCarousel: Component<Props> = (props) => {
                                         currBtn.classList.remove('selected')
                                     }
 
+                                    console.log("color scheme: ", light, dark)
+
                                 }}
                             >
-                                <img src={item.icon} alt={item.ariaLabel} title={item.description} class="w-8" />
+
+
+                                { light && <img src={ item.icon } alt={item.ariaLabel} title={item.description} class="w-8" /> }
+                                { dark && <img src={ item.iconDM } alt={item.ariaLabel} title={item.description} class="w-8" /> }
+                               
+                                {/* <img src={ item.iconDM } /> */}
+                                {/* <img src={ item.icon } alt={item.ariaLabel} title={item.description} class="w-8" />
+                                <img src={ item.iconDM } alt={item.ariaLabel} title={item.description} class="w-8" /> */}
                                 <p class=" text-text1 dark:text-text1-DM my-2 text-center text-xs">{item.name} </p>
-                                <img src=" " alt="line" class="hidden"/>
+
+                                {/* <svg width="800px" height="800px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" class="fill-logo dark:fill-logo-DM">
+                                    <title>palette</title>
+                                    <g id="Layer_2" data-name="Layer 2">
+                                        <g id="invisible_box" data-name="invisible box">
+                                        <rect width="48" height="48" fill="none"/>
+                                        </g>
+                                        <g id="icons_Q2" data-name="icons Q2">
+                                        <path d="M26.6,6H27c8.1,0,15,5.5,15,12S40.4,29,28,29c-3.8,0-5.8,2.2-6.4,4.3a5.5,5.5,0,0,0,2.3,6.1c1,.6,1,1.3.9,1.7a1.2,1.2,0,0,1-1.3.9C11.4,42,6,33,6,24S15.2,6,26.5,6h.1m-.1-4C13,2,2,11.8,2,24s8,22,21.5,22C29,46,31,39,26,36c-.9-.6-1-3,2-3,9,0,18-3,18-15C46,9,37,2,27,2Z"/>
+                                        <path d="M21,10a3,3,0,1,0,3,3,2.9,2.9,0,0,0-3-3Z"/>
+                                        <path d="M14,15a3,3,0,1,0,3,3,2.9,2.9,0,0,0-3-3Z"/>
+                                        <path d="M29,9a3,3,0,1,0,3,3,2.9,2.9,0,0,0-3-3Z"/>
+                                        <path d="M36,14a3,3,0,1,0,3,3,2.9,2.9,0,0,0-3-3Z"/>
+                                        </g>
+                                    </g>
+                                </svg> */}
                             </button>
                             ))
                         }
