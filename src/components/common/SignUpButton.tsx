39a53c23c67e4c8171fea4979aadd8630344bbb5
Meagan <meagan.h.smith@gmail.com>
2023-08-09 13:47:48 -0400

    Add basic text and styles to index file, create signup button component

---
+++ src/components/common/SignUpButton.tsx
@@ -0,0 +1,28 @@
+import { Component } from 'solid-js';
+import { getLangFromUrl, useTranslations } from '../../i18n/utils';
+import { supabase } from '../../lib/supabaseClient'
+import { useStore } from '@nanostores/solid'
+
+import '../../styles/global.css';
+
+const lang = getLangFromUrl(new URL(window.location.href));
+const t = useTranslations(lang);
+
+const signup = () => {
+    window.location.href = "/signup"
+}
+
+export const SignUpBtn: Component = () => {
+
+    return (
+        <div>
+            <button 
+                class="btn-primary ml-2" 
+                type="submit" 
+                onclick={ signup }
+            >
+                {t('pageTitles.signUp')}
+            </button>
+        </div>
+    )
+}
