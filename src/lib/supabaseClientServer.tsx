9bdf4c682c37292417e309a5e5a7691c25767b70
r-southworth <rachael.southworth@datagrove.com>
2023-08-28 13:25:27 -0400

    adjust server supabase client

--- src/lib/supabaseClientServer.tsx
+++ src/lib/supabaseClientServer.tsx
@@ -8,7 +8,7 @@ export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKe
         fetch: (...args) => fetch(...args),
       },
     auth: {
-        storage: localStorage,
+        storage: undefined,
         autoRefreshToken: true,
         persistSession: false,
         detectSessionInUrl: true,
