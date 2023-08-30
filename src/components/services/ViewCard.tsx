fc7cb7e34868beef1eb9b178b3b0a0430755059f
r-southworth <rachael.southworth@datagrove.com>
2023-08-30 17:14:50 -0400

    update card styles

--- src/components/services/ViewCard.tsx
+++ src/components/services/ViewCard.tsx
@@ -1,7 +1,7 @@
 import { Component, createSignal, createEffect } from "solid-js";
 import { DeletePostButton } from "../posts/DeletePostButton";
 import { supabase } from "../../lib/supabaseClient";
-import { getLangFromUrl, useTranslations } from '../../i18n/utils';
+import { getLangFromUrl, useTranslations } from "../../i18n/utils";
 
 const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
@@ -31,17 +31,17 @@ export const ViewCard: Component<Props> = (props) => {
     if (props.posts) {
       const updatedPosts = await Promise.all(
         props.posts.map(async (post: any) => {
-          post.image_urls ? (
-            post.image_url = await downloadImage(post.image_urls.split(',')[0])
-          ) : (
-            post.image_url = null
-          );
+          post.image_urls
+            ? (post.image_url = await downloadImage(
+                post.image_urls.split(",")[0]
+              ))
+            : (post.image_url = null);
           return post;
         })
       );
-      
+
       setNewPosts(updatedPosts);
-    };
+    }
   });
 
   const downloadImage = async (path: string) => {
@@ -61,21 +61,24 @@ export const ViewCard: Component<Props> = (props) => {
     }
   };
 
-
   return (
     <div class="flex justify-center w-full">
       <ul class="md:flex md:flex-wrap md:justify-center md:w-full">
         {newPosts().map((post: any) => (
           <li class=" w-[99%]">
             <a href={`/${lang}/posts/${post.id}`}>
-              <div class="mb-2 flex flex-col md:flex-row md:justify-start justify-center items-center rounded-lg md:h-48 shadow-lg shadow-shadow-LM dark:shadow-shadow-DM">
+              <div class="mb-2 flex flex-col md:flex-row md:justify-start justify-center items-center rounded-lg md:h-48 shadow-md shadow-shadow-LM dark:shadow-shadow-DM box-content border border-border1 border-opacity-25">
                 <div class="flex md:w-48 w-full h-80 md:h-48 md:mr-2 items-center justify-center bg-background1 dark:bg-background1-DM rounded-lg">
                   {post.image_url ? (
                     <img
                       src={post.image_url}
-                      alt={post.image_urls.split(',')[0] ? "User Image" : "No image"}
-                      class="bg-background1 dark:bg-icon1-DM rounded-lg md:shadow-lg dark:shadow-2xl w-full h-full object-cover"
-                    // style={{height: `120px`, width: `120px`}}
+                      alt={
+                        post.image_urls.split(",")[0]
+                          ? "User Image"
+                          : "No image"
+                      }
+                      class="bg-background1 dark:bg-icon1-DM rounded-lg w-full h-full object-cover"
+                      // style={{height: `120px`, width: `120px`}}
                     />
                   ) : (
                     <svg
@@ -84,8 +87,14 @@ export const ViewCard: Component<Props> = (props) => {
                       class="dark:bg-icon1-DM fill-logo rounded-lg w-full h-full object-cover"
                     >
                       <g id="Page-1" stroke="none" stroke-width="1">
-                        <g id="icon" transform="translate(64.000000, 64.000000)">
-                          <path d="M384,1.42108547e-14 L384,384 L1.42108547e-14,384 L1.42108547e-14,1.42108547e-14 L384,1.42108547e-14 Z M109.226667,142.933333 L42.666,249.881 L42.666,341.333 L341.333,341.333 L341.333,264.746 L277.333333,200.746667 L211.84,266.24 L109.226667,142.933333 Z M245.333333,85.3333333 C227.660221,85.3333333 213.333333,99.6602213 213.333333,117.333333 C213.333333,135.006445 227.660221,149.333333 245.333333,149.333333 C263.006445,149.333333 277.333333,135.006445 277.333333,117.333333 C277.333333,99.6602213 263.006445,85.3333333 245.333333,85.3333333 Z" id="Combined-Shape"></path>
+                        <g
+                          id="icon"
+                          transform="translate(64.000000, 64.000000)"
+                        >
+                          <path
+                            d="M384,1.42108547e-14 L384,384 L1.42108547e-14,384 L1.42108547e-14,1.42108547e-14 L384,1.42108547e-14 Z M109.226667,142.933333 L42.666,249.881 L42.666,341.333 L341.333,341.333 L341.333,264.746 L277.333333,200.746667 L211.84,266.24 L109.226667,142.933333 Z M245.333333,85.3333333 C227.660221,85.3333333 213.333333,99.6602213 213.333333,117.333333 C213.333333,135.006445 227.660221,149.333333 245.333333,149.333333 C263.006445,149.333333 277.333333,135.006445 277.333333,117.333333 C277.333333,99.6602213 263.006445,85.3333333 245.333333,85.3333333 Z"
+                            id="Combined-Shape"
+                          ></path>
                         </g>
                       </g>
                     </svg>
@@ -93,23 +102,49 @@ export const ViewCard: Component<Props> = (props) => {
                 </div>
 
                 {/* <br /> */}
-                <div id="cardContent" class="px-1 pt-1 text-left w-full md:w-5/6 md:h-full">
+                <div
+                  id="cardContent"
+                  class="px-1 pt-1 text-left w-full md:w-5/6 md:h-full"
+                >
                   <div class="grid grid-cols-4">
-                    <p class="text-2xl font-bold mb-2 text-ptext1 dark:text-ptext1-DM overflow-hidden max-h-14 col-span-3">
+                    <div class="relative col-span-4 w-full flex align-top md:mt-2">
+                      <div class="truncate inline-block max-w-[58%] text-ptext2 dark:text-ptext2-DM text-sm md:text-base bg-background2 dark:bg-background2-DM  opacity-50 dark:opacity-100 w-fit rounded-lg px-2">
+                        {post.major_municipality}/{post.minor_municipality}/
+                        {post.governing_district}
+                      </div>
+                      <div class="truncate inline-block max-w-[28%] text-ptext2 dark:text-ptext2-DM text-sm md:text-base bg-background2 dark:bg-background2-DM  opacity-50 dark:opacity-100 w-fit rounded-lg px-2 ml-1">
+                        {post.category}
+                      </div>
+                      <div class="absolute right-2 inline-block">
+                        <DeletePostButton
+                          id={post.id}
+                          userId={post.user_id}
+                          postImage={post.image_urls}
+                        />
+                      </div>
+                    </div>
+
+                    <p class="text-2xl font-bold text-ptext1 dark:text-ptext1-DM overflow-hidden max-h-14 col-span-4 pr-4 truncate">
                       {post.title}
                     </p>
-                    <div class="justify-self-end pt-2 pr-4">
-                      <DeletePostButton id={post.id} userId={post.user_id} postImage={post.image_urls} />
-                    </div>
+                    {/* <div class="justify-self-end pt-2 pr-4">
+                      <DeletePostButton
+                        id={post.id}
+                        userId={post.user_id}
+                        postImage={post.image_urls}
+                      />
+                    </div> */}
                   </div>
-                  
-                  <p class="overflow-hidden text-ptext1 dark:text-ptext1-DM text-base"><span class="font-bold">{t('postLabels.provider')}</span>{post.provider_name}</p>
-                  <p class="overflow-hidden text-ptext1 dark:text-ptext1-DM text-base">
-                    <span class="font-bold">{t('postLabels.location')}</span>{post.major_municipality}/{post.minor_municipality}/
-                    {post.governing_district}
+
+                  <p class="overflow-hidden text-ptext1 dark:text-ptext1-DM text-base mb-1">
+                    <span class="font-bold">{t("postLabels.provider")}</span>
+                    {post.provider_name}
                   </p>
-                  <p class="overflow-hidden text-ptext1 dark:text-ptext1-DM pt-1 text-base">{t('postLabels.category')}{post.category}</p>
-                  <p class=" text-ptext1 dark:text-ptext1-DM text-sm max-h-[60px] md:h-[40px] md:line-clamp-2 xl:line-clamp-3 mb-2 overflow-hidden mr-4" innerHTML={post.content}></p>
+
+                  <p
+                    class=" text-ptext1 dark:text-ptext1-DM text-sm max-h-[60px] line-clamp-3 mb-2 overflow-hidden mr-4"
+                    innerHTML={post.content}
+                  ></p>
                 </div>
               </div>
             </a>
