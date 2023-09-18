129dacfdbc036001d873b67ec8da04151e344d1a
Meagan <meagan.h.smith@gmail.com>
2023-09-18 12:48:45 -0500

    Fix dark mode color for email icon

--- src/components/posts/SocialMediaShares.tsx
+++ src/components/posts/SocialMediaShares.tsx
@@ -138,7 +138,7 @@ export const SocialMediaShares: Component<Props> = (props) => {
                 </div>
 
                 <div id="email-share" class="flex justify-center items-center">
-                    <button id="button--email" class="email-share-button p-1 rounded font-bold text-icon1 dark:text-icon1-DM1" title="Share to Email" onclick={ emailShare }>
+                    <button id="button--email" class="email-share-button p-1 rounded font-bold text-icon1 dark:text-icon1-DM" title="Share to Email" onclick={ emailShare }>
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail-forward" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M12 18h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5"></path>
