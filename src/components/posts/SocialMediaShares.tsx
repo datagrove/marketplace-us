3ce1493d4526cc2fe432b2dca007013816eafbc1
Meagan <meagan.h.smith@gmail.com>
2023-09-20 10:50:36 -0500

    Update icon for copy link

--- src/components/posts/SocialMediaShares.tsx
+++ src/components/posts/SocialMediaShares.tsx
@@ -189,12 +189,10 @@ export const SocialMediaShares: Component<Props> = (props) => {
 
                     <div id="link-share" class="socialBtn">
                         <button id="button--link" class="link-share-button p-1 rounded font-bold text-icon1 dark:text-icon1-DM" title="Copy Link" onclick={ (e) => linkShare(e) }>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail-forward" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M12 18h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5"></path>
-                                <path d="M3 6l9 6l9 -6"></path>
-                                <path d="M15 18h6"></path>
-                                <path d="M18 15l3 3l-3 3"></path>
+                                <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+                                <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
                             </svg>
                         </button>
                         <p class="hidden md:inline">Copy Link</p>
