49cf14a2b81333086a0d26a42472beb632cf37d6
r-southworth <rachael.southworth@datagrove.com>
2023-09-25 18:00:12 -0400

    separate component working to validate number

--- src/components/users/forms/Phone.tsx
+++ src/components/users/forms/Phone.tsx
@@ -1,62 +1,49 @@
 import {
   Component,
-  Suspense,
   createEffect,
-  createResource,
   createSignal,
-  onMount,
   onCleanup,
+  onMount,
 } from "solid-js";
 import "intl-tel-input/build/css/intlTelInput.css"; // Import the CSS for styling
 import intlTelInput from "intl-tel-input";
 
-
-
 function TelephoneInput() {
   const [telephoneValue, setTelephoneValue] = createSignal("");
-  let inputRef: any;
+  let inputRef: HTMLInputElement | null = null;
+  let telInput: any = null;
 
   onMount(() => {
     const inputElement = inputRef;
-    console.log(inputElement);
+
     if (inputElement) {
-      // Wrap intlTelInput initialization in a Promise
-      const telInputPromise = new Promise((resolve) => {
-        intlTelInput(inputElement, {
-          initialCountry: "us",
-          separateDialCode: false,
-          utilsScript: "/intl-tel-input/build/js/utils.js", // Make sure to provide the correct path to utils.js
-        }),
-          () => {
-            resolve(intlTelInputGlobals.getInstance(inputElement)); // Resolve with the initialized instance
-          };
+      telInput = intlTelInput(inputElement, {
+        initialCountry: "us",
+        separateDialCode: false,
+        utilsScript: "/intl-tel-input/build/js/utils.js", // Make sure to provide the correct path to utils.js
       });
 
-      telInputPromise.then((telInput) => {
-        console.log("Instance: " + telInput);
+      // Event listener to update the signal when the input value changes
+      const inputHandler = () => {
+        setTelephoneValue(telInput.getNumber());
+        console.log(telInput.isPossibleNumber());
+      };
 
-        // Event listener to update the signal when the input value changes
-        inputElement.addEventListener("input", () => {
-          console.log("TelInput: " + telInput.getNumber());
-          console.log("Input Value: " + inputElement.value);
-          setTelephoneValue(telInput.getNumber());
-        });
+      inputElement.addEventListener("input", inputHandler);
 
-        onCleanup(() => {
+      onCleanup(() => {
+        if (telInput) {
           telInput.destroy();
-          inputElement.removeEventListener();
-        });
+        }
+        inputElement.removeEventListener("input", inputHandler);
       });
-
-      // Cleanup function when the component is unmounted
-      
     }
   });
 
   return (
     <div>
       <input
-        ref={inputRef}
+        ref={(el) => (inputRef = el)}
         type="tel"
         id="telephoneInput"
         placeholder="Enter phone number"
