c396c7396e1e1e745079e7dac12fe6025f4dfd31
r-southworth <rachael.southworth@datagrove.com>
2023-10-16 16:18:25 -0400

    fix supabase client and seed.sql

--- src/lib/supabaseClient.tsx
+++ src/lib/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.PUBLIC_VITE_SUPABASE_LOCAL_URL
-const supabaseAnonKey = import.meta.env.PUBLIC_VITE_SUPABASE_LOCAL_ANON_KEY
+const supabaseUrl = import.meta.env.PUBLIC_VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_VITE_SUPABASE_ANON_KEY
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
     global: {
