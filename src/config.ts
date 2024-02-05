3ab59b7a3c6ef751b545c19f6593c83fb5334bd6
germanzarkovich <germanzarkovich@gmail.com>
2024-02-05 08:45:13 -0500

    refactor language files

--- src/config.ts
+++ src/config.ts
@@ -4,27 +4,29 @@
 // const t = useTranslations(lang);
 
 const CONFIG = {
-    name: 'TodoServis',
-  
-    title: 'TodoServis',
-    description: 'Everything you need to get the job done.',
-    url: 'https://todoservis.com/',
-    devUrl: 'http://localhost:4321',
-    pagesDevUrl: 'https://pwa.marketplace-4xm.pages.dev',
-  
-    defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
-    themeColor: '#1DD762',
-  
-    language: 'es',
-    textDirection: 'ltr',
-  
-    dateFormatter: new Intl.DateTimeFormat('en', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-      timeZone: 'UTC',
-    }),
-  };
-  
-  export const SITE = { ...CONFIG};
-  export const DATE_FORMATTER = CONFIG.dateFormatter;
+  name: "HomeSchool",
+
+  title: "HomeSchool",
+  description: "HomeSchool description",
+  //NEED TO ADD NEW URL
+  url: "https://NEWURL.com/",
+  devUrl: "http://localhost:4321",
+  //NEED TO ADD NEW URL
+  pagesDevUrl: "https://pwa.NEWURL.pages.dev",
+
+  defaultTheme: "system", // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
+  themeColor: "#1DD762",
+
+  language: "es",
+  textDirection: "ltr",
+
+  dateFormatter: new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: "UTC",
+  }),
+};
+
+export const SITE = { ...CONFIG };
+export const DATE_FORMATTER = CONFIG.dateFormatter;
