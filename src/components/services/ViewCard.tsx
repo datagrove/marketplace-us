0a7e79c0fe8cb6307baf43293ea7c411132b8e0d
Meagan <meagan.h.smith@gmail.com>
2024-04-01 14:11:50 -0500

    Fix dark mode fills for card icons

--- src/components/services/ViewCard.tsx
+++ src/components/services/ViewCard.tsx
@@ -101,7 +101,8 @@ export const ViewCard: Component<Props> = (props) => {
                       class="bg-background1 dark:bg-icon1-DM rounded-lg w-full h-full object-cover"
                     />
                   ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="800px" height="800px" viewBox="35 0 186 256" id="Flat">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="35 0 186 256" id="Flat" class="fill-icon1 dark:fill-icon1-DM">
+
                       <path d="M208,36H48A12.01312,12.01312,0,0,0,36,48V208a12.01312,12.01312,0,0,0,12,12H208a12.01312,12.01312,0,0,0,12-12V48A12.01312,12.01312,0,0,0,208,36Zm4,172a4.004,4.004,0,0,1-4,4H48a4.004,4.004,0,0,1-4-4V177.65631l33.17187-33.171a4.00208,4.00208,0,0,1,5.65723,0l20.68652,20.68652a12.011,12.011,0,0,0,16.96973,0l44.68652-44.68652a4.00208,4.00208,0,0,1,5.65723,0L212,161.65625Zm0-57.65625L176.48535,114.8291a11.99916,11.99916,0,0,0-16.96973,0L114.8291,159.51562a4.00681,4.00681,0,0,1-5.65723,0L88.48535,138.8291a12.01009,12.01009,0,0,0-16.96973,0L44,166.34393V48a4.004,4.004,0,0,1,4-4H208a4.004,4.004,0,0,1,4,4ZM108.001,92v.00195a8.001,8.001,0,1,1,0-.00195Z"/>
                     </svg>
                   )}
@@ -134,7 +135,7 @@ export const ViewCard: Component<Props> = (props) => {
                             alt="Seller image"
                           />
                         ) : (
-                          <svg fill="#000000" width="24px" height="24px" class="rounded-full border-2 border-border1 dark:border-border1-DM mr-1" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/></svg>
+                          <svg width="24px" height="24px" class="rounded-full border-2 border-border1 dark:border-border1-DM mr-1 fill-icon1 dark:bg-icon1-DM" viewBox="0 0 32 32"><path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/></svg>
                           
                         )}
                         <p class="overflow-hidden text-ptext1 font-light dark:text-ptext1-DM text-base mb-1">{post.seller_name}</p>
@@ -172,7 +173,7 @@ export const ViewCard: Component<Props> = (props) => {
                     <p class="font-bold text-lg">${post.price.toFixed(2)} </p>
 
                     <div class="reviews-div  w-full flex justify-end text-end items-center">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="12px" height="12px" viewBox="0 0 32 32"><script/><path d="M 30.335938 12.546875 L 20.164063 11.472656 L 16 2.132813 L 11.835938 11.472656 L 1.664063 12.546875 L 9.261719 19.394531 L 7.140625 29.398438 L 16 24.289063 L 24.859375 29.398438 L 22.738281 19.394531 Z"/><script/></svg>
+                      <svg width="12px" height="12px" viewBox="0 0 32 32" class="fill-icon1 dark:fill-icon1-DM"><script/><path d="M 30.335938 12.546875 L 20.164063 11.472656 L 16 2.132813 L 11.835938 11.472656 L 1.664063 12.546875 L 9.261719 19.394531 L 7.140625 29.398438 L 16 24.289063 L 24.859375 29.398438 L 22.738281 19.394531 Z"/><script/></svg>
 
                       <p class="text-xs ml-1">4.9 (30.3K)</p>
                     </div>
@@ -182,9 +183,9 @@ export const ViewCard: Component<Props> = (props) => {
 
                   <div class="fileTypes-div flex flex-col w-full items-center justify-end">
                     <div class="w-full flex justify-end items-center">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 28 28" version="1.1">
-                        <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g id="ic_fluent_checkmark_28_filled" fill="#212121" fill-rule="nonzero">
+                      <svg width="16px" height="16px" viewBox="0 0 28 28" version="1.1" class="fill-icon1 dark:fill-icon1-DM">
+                        <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" class="fill-icon1 dark:fill-icon1-DM">
+                            <g id="ic_fluent_checkmark_28_filled" fill-rule="nonzero">
                               <path d="M10.5,19.5857864 L4.20710678,13.2928932 C3.81658249,12.9023689 3.18341751,12.9023689 2.79289322,13.2928932 C2.40236893,13.6834175 2.40236893,14.3165825 2.79289322,14.7071068 L9.79289322,21.7071068 C10.1834175,22.0976311 10.8165825,22.0976311 11.2071068,21.7071068 L25.2071068,7.70710678 C25.5976311,7.31658249 25.5976311,6.68341751 25.2071068,6.29289322 C24.8165825,5.90236893 24.1834175,5.90236893 23.7928932,6.29289322 L10.5,19.5857864 Z" id="🎨-Color">
                               </path>
                             </g>
@@ -195,9 +196,9 @@ export const ViewCard: Component<Props> = (props) => {
                     </div>
 
                     <div class="w-full flex justify-end items-center">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 28 28" version="1.1">
-                        <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g id="ic_fluent_checkmark_28_filled" fill="#212121" fill-rule="nonzero">
+                      <svg width="16px" height="16px" viewBox="0 0 28 28" version="1.1" class="fill-icon1 dark:fill-icon1-DM">
+                        <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" class="fill-icon1 dark:fill-icon1-DM">
+                            <g id="ic_fluent_checkmark_28_filled" fill-rule="nonzero">
                               <path d="M10.5,19.5857864 L4.20710678,13.2928932 C3.81658249,12.9023689 3.18341751,12.9023689 2.79289322,13.2928932 C2.40236893,13.6834175 2.40236893,14.3165825 2.79289322,14.7071068 L9.79289322,21.7071068 C10.1834175,22.0976311 10.8165825,22.0976311 11.2071068,21.7071068 L25.2071068,7.70710678 C25.5976311,7.31658249 25.5976311,6.68341751 25.2071068,6.29289322 C24.8165825,5.90236893 24.1834175,5.90236893 23.7928932,6.29289322 L10.5,19.5857864 Z" id="🎨-Color">
                               </path>
                             </g>
@@ -208,9 +209,9 @@ export const ViewCard: Component<Props> = (props) => {
                     </div>
 
                     <div class="w-full flex justify-end items-center">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 28 28" version="1.1">
-                        <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g id="ic_fluent_checkmark_28_filled" fill="#212121" fill-rule="nonzero">
+                      <svg width="16px" height="16px" viewBox="0 0 28 28" version="1.1" class="fill-icon1 dark:fill-icon1-DM">
+                        <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" class="fill-icon1 dark:fill-icon1-DM">
+                            <g id="ic_fluent_checkmark_28_filled" fill-rule="nonzero">
                               <path d="M10.5,19.5857864 L4.20710678,13.2928932 C3.81658249,12.9023689 3.18341751,12.9023689 2.79289322,13.2928932 C2.40236893,13.6834175 2.40236893,14.3165825 2.79289322,14.7071068 L9.79289322,21.7071068 C10.1834175,22.0976311 10.8165825,22.0976311 11.2071068,21.7071068 L25.2071068,7.70710678 C25.5976311,7.31658249 25.5976311,6.68341751 25.2071068,6.29289322 C24.8165825,5.90236893 24.1834175,5.90236893 23.7928932,6.29289322 L10.5,19.5857864 Z" id="🎨-Color">
                               </path>
                             </g>
