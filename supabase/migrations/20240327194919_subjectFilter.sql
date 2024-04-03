b7f66d8ae2548212046b2ea4193e598e13a534ba
germanzarkovich <germanzarkovich@gmail.com>
2024-04-03 12:28:54 -0400

    Add rachael solution to the problem in the migration and seed files

--- supabase/migrations/20240327194919_subjectFilter.sql
+++ supabase/migrations/20240327194919_subjectFilter.sql
@@ -67,7 +67,7 @@ alter table "public"."post_subject" enable row level security;
 
 alter table "public"."seller_post" drop column "product_category" CASCADE;
 
-alter table "public"."seller_post" add column "product_subject" bigint not null;
+alter table "public"."seller_post" add column "product_subject" text[] not null;
 
 CREATE UNIQUE INDEX post_subject_id_key ON public.post_subject USING btree (id);
 
@@ -81,9 +81,9 @@ alter table "public"."post_subject" add constraint "post_subject_language_fkey" 
 
 alter table "public"."post_subject" validate constraint "post_subject_language_fkey";
 
-alter table "public"."seller_post" add constraint "seller_post_product_subject_fkey" FOREIGN KEY (product_subject) REFERENCES post_subject(id) not valid;
+-- alter table "public"."seller_post" add constraint "seller_post_product_subject_fkey" FOREIGN KEY (product_subject) REFERENCES post_subject(id) not valid;
 
-alter table "public"."seller_post" validate constraint "seller_post_product_subject_fkey";
+-- alter table "public"."seller_post" validate constraint "seller_post_product_subject_fkey";
 
 create or replace view "public"."sellerposts" as  SELECT seller_post.id,
     seller_post.title,
@@ -161,31 +161,11 @@ using (true);
 
 
 
-alter table "public"."seller_post"drop constraint"seller_post_product_subject_fkey";
+-- alter table "public"."seller_post"drop constraint"seller_post_product_subject_fkey";
 
-drop view if exists"public"."sellerposts" cascade;
+-- drop view if exists"public"."sellerposts" cascade;
 
--- alter table "public"."seller_post" alter column "product_subject" set data type text using "product_subject"::text;
-
-alter table "public"."seller_post" alter column "product_subject" set data type text[] using "product_subject"::text[];
-create or replace view "public"."sellerposts" as  SELECT seller_post.id,
-    seller_post.title,
-    seller_post.content,
-    seller_post.user_id,
-    seller_post.image_urls,
-    seller_post.product_subject,
-    locationview.major_municipality,
-    locationview.minor_municipality,
-    locationview.governing_district,
-    sellers.seller_name,
-    sellers.seller_id,
-    profiles.email,
-    seller_post.stripe_price_id AS price_id,
-    seller_post.stripe_product_id AS product_id
-   FROM (((seller_post
-     LEFT JOIN profiles ON ((seller_post.user_id = profiles.user_id)))
-     LEFT JOIN sellers ON ((seller_post.user_id = sellers.user_id)))
-     LEFT JOIN locationview ON ((seller_post.location = locationview.id)));
+-- alter table "public"."seller_post" alter column "product_subject" set data type text[] using "product_subject"::text[];
 
 
   
