c8d7d7137f6ffb9fc499e059e73ce7a20fce4340
germanzarkovich <germanzarkovich@gmail.com>
2024-04-09 14:21:31 -0400

    add all new svgs

--- src/components/services/CategoryCarousel.tsx
+++ src/components/services/CategoryCarousel.tsx
@@ -13,6 +13,14 @@ const productCategoryData = values.productCategoryInfo;
 import rightArrow from "../../assets/categoryIcons/circled-right-arrow.svg";
 import leftArrow from "../../assets/categoryIcons/circled-left-arrow.svg";
 import garden from "../../assets/categoryIcons/garden.svg";
+import history from "../../assets/categoryIcons/history.svg";
+import art from "../../assets/categoryIcons/art.svg";
+import geography from "../../assets/categoryIcons/geography.svg";
+import math from "../../assets/categoryIcons/math.svg";
+import science from "../../assets/categoryIcons/science.svg";
+import specialty from "../../assets/categoryIcons/specialty.svg";
+import holiday from "../../assets/categoryIcons/history.svg";
+import social from "../../assets/categoryIcons/social.svg";
 // import travel from "../../assets/categoryIcons/travel.svg";
 // import worker from "../../assets/categoryIcons/worker.svg";
 // import palette from "../../assets/categoryIcons/palette.svg";
@@ -44,24 +52,23 @@ if (error) {
 
 categories.map((category) => {
   if (category.id === 1) {
-    category.icon = garden;
+    category.icon = geography;
+  } else if (category.id === 2) {
+    category.icon = history;
+  } else if (category.id === 3) {
+    category.icon = art;
+  } else if (category.id === 4) {
+    category.icon = holiday;
+  } else if (category.id === 5) {
+    category.icon = math;
+  } else if (category.id === 6) {
+    category.icon = science;
+  } else if (category.id === 7) {
+    category.icon = social;
+  } else if (category.id === 8) {
+    category.icon = specialty;
   }
-  //   else if (category.id === 2) {
-  //   category.icon = beauty;
-  // } else if (category.id === 3) {
-  //   category.icon = construction;
-  // } else if (category.id === 4) {
-  //   category.icon = computer;
-  // } else if (category.id === 5) {
-  //   category.icon = engine;
-  // } else if (category.id === 6) {
-  //   category.icon = palette;
-  // } else if (category.id === 7) {
-  //   category.icon = finance;
-  //   category.iconDM = financeDM;
-  // } else if (category.id === 8) {
-  //   category.icon = cleaner;
-  // } else if (category.id === 9) {
+  // else if (category.id === 9) {
   //   category.icon = paw;
   // } else if (category.id === 10) {
   //   category.icon = legal;
@@ -82,7 +89,7 @@ for (let i = 0; i < categoriesData.length; i++) {
   allCategoryInfo.push({
     ...categoriesData[i],
     ...categories.find(
-      (itmInner) => itmInner.id.toString() === categoriesData[i].id
+      (itmInner) => itmInner.id.toString() === categoriesData[i].id,
     ),
   });
 }
@@ -94,22 +101,22 @@ interface Props {
 
 let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 let light = window.matchMedia(
-  "(prefers-color-scheme: light)" || "(prefers-color-scheme: no-preference"
+  "(prefers-color-scheme: light)" || "(prefers-color-scheme: no-preference",
 ).matches;
 
 export const CategoryCarousel: Component<Props> = (props) => {
   return (
-    <div class="product-carousel my-2 rounded-lg p-1">
+    <div class="p-1 my-2 rounded-lg product-carousel">
       <div class="flex flex-start justify-between scrollbar-thin overflow-x-auto drop-shadow-md dark:drop-shadow-[0_4px_3px_rgba(97,97,97,1)]  scrollbar-track-rounded-full scrollbar-thumb-rounded-full scrollbar-thumb-shadow-LM scrollbar-track-background1 dark:scrollbar-thumb-shadow-DM dark:scrollbar-track-background1-DM">
-        <button class="w-12 hidden">
+        <button class="hidden w-12">
           <img src={leftArrow.src} alt="Left Arrow" />
         </button>
 
-        <div class="flex justify-between items-start w-full pt-2 h-[7.5rem]">
+        <div class="flex justify-between items-start pt-2 w-full h-[7.5rem]">
           {allCategoryInfo?.map((item) => (
             <button
               id={item.id}
-              class="catBtn flex flex-col flex-none justify-start items-center w-20 h-28"
+              class="flex flex-col flex-none justify-start items-center w-20 h-28 catBtn"
               onClick={(e) => {
                 props.filterPosts(item.id);
 
@@ -122,19 +129,19 @@ export const CategoryCarousel: Component<Props> = (props) => {
                 }
               }}
             >
-              <div class="bg-iconbg1 dark:bg-iconbg1-DM rounded-full">
-              {item.icon && item.icon.src ? (
+              <div class="rounded-full bg-iconbg1 dark:bg-iconbg1-DM">
+                {item.icon && item.icon.src ? (
                   <img
                     src={item.icon.src}
                     alt={item.ariaLabel + " Icon"}
                     title={item.description}
-                    class="w-12 h-12 p-1 m-2"
+                    class="p-1 m-2 w-12 h-12"
                   />
                 ) : null}
               </div>
 
               <div class="flex flex-row justify-center items-center h-44">
-                <p class="text-ptext1 dark:text-ptext2-DM text-center text-xs">
+                <p class="text-xs text-center text-ptext1 dark:text-ptext2-DM">
                   {item.name}{" "}
                 </p>
               </div>
@@ -142,7 +149,7 @@ export const CategoryCarousel: Component<Props> = (props) => {
           ))}
         </div>
 
-        <button class="w-12 hidden">
+        <button class="hidden w-12">
           <img src={rightArrow.src} alt="Right Arrow" />
         </button>
       </div>
