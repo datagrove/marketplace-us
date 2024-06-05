ee695443137d41214f70f41ad3802770d9dbd456
r-southworth <rachael.southworth@datagrove.com>
2024-06-05 14:37:12 -0400

    header tweaks

--- src/components/common/HeaderSearchBar.tsx
+++ src/components/common/HeaderSearchBar.tsx
@@ -36,14 +36,15 @@ export const SearchBar: Component = () => {
                     // // Cancel the default action, if needed
                     // e.preventDefault();
                     // Trigger the button element with a click
+                    console.log(searchString());
                     clickSearch(e);
                 }
             });
     });
 
     return (
-        <div class="search-form w-full mx-4 flex justify-center items-center">
-            <div class="w-full h-3/4 flex justify-between items-center form rounded-full border border-border1 px-1 text-ptext1  focus:border-2 focus:border-highlight1 focus:outline-none dark:border-border1-DM dark:focus:border-highlight1-DM">
+        <div class="search-form w-full h-full mx-4 flex justify-center items-center">
+            <div class="w-full h-full flex justify-between items-center form rounded-full border border-border1 px-1 text-ptext1  focus:border-2 focus:border-highlight1 focus:outline-none dark:border-border1-DM dark:focus:border-highlight1-DM">
                 <label class="sr-only" for="search">
                     {t("formLabels.search")}
                 </label>
