41a1fc547e665581dd658c193b482a780a41c53b
gerzarko <germanzarkovich@gmail.com>
2024-06-10 13:39:55 -0400

    add fixes with rachael's help

--- src/components/services/ViewCard.tsx
+++ src/components/services/ViewCard.tsx
@@ -138,7 +138,7 @@ export const ViewCard: Component<Props> = (props) => {
                     <li class="w-[99%]">
                         <a href={`/${lang}/posts/${post.id}`}>
                             <div class="mb-2 box-content flex w-full flex-col items-center justify-center rounded-lg border border-border1 border-opacity-25 shadow-md shadow-shadow-LM dark:border-border1-DM dark:border-opacity-25 dark:shadow-shadow-DM md:h-48 md:flex-row md:items-start md:justify-start">
-                                <div class="flex h-36 w-36 items-center justify-center rounded-lg bg-background1 dark:bg-background1-DM md:mr-2 md:h-48 md:w-48">
+                                <div class="flex items-center justify-center rounded-lg bg-background1 dark:bg-background1-DM md:mr-2 md:h-48 md:w-48 md:shrink-0">
                                     {post.image_url ? (
                                         <img
                                             src={post.image_url}
@@ -152,11 +152,9 @@ export const ViewCard: Component<Props> = (props) => {
                                     ) : (
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
-                                            width="800px"
-                                            height="800px"
                                             viewBox="35 0 186 256"
                                             id="Flat"
-                                            class="fill-icon1 dark:fill-icon1-DM"
+                                            class="h-full w-full fill-icon1 dark:fill-icon1-DM"
                                         >
                                             <path d="M208,36H48A12.01312,12.01312,0,0,0,36,48V208a12.01312,12.01312,0,0,0,12,12H208a12.01312,12.01312,0,0,0,12-12V48A12.01312,12.01312,0,0,0,208,36Zm4,172a4.004,4.004,0,0,1-4,4H48a4.004,4.004,0,0,1-4-4V177.65631l33.17187-33.171a4.00208,4.00208,0,0,1,5.65723,0l20.68652,20.68652a12.011,12.011,0,0,0,16.96973,0l44.68652-44.68652a4.00208,4.00208,0,0,1,5.65723,0L212,161.65625Zm0-57.65625L176.48535,114.8291a11.99916,11.99916,0,0,0-16.96973,0L114.8291,159.51562a4.00681,4.00681,0,0,1-5.65723,0L88.48535,138.8291a12.01009,12.01009,0,0,0-16.96973,0L44,166.34393V48a4.004,4.004,0,0,1,4-4H208a4.004,4.004,0,0,1,4,4ZM108.001,92v.00195a8.001,8.001,0,1,1,0-.00195Z" />
                                         </svg>
