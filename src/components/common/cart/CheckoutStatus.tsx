3c41fb75a82b6f0d84b0396444448a9407692c8d
Meagan <meagan.h.smith@gmail.com>
2024-06-17 15:01:15 -0400

    Comment code back in for session user

--- src/components/common/cart/CheckoutStatus.tsx
+++ src/components/common/cart/CheckoutStatus.tsx
@@ -15,32 +15,6 @@ import type { Post } from "@lib/types";
 const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
 
-const testPosts = {
-    id: 1,
-    title: "test post for styling",
-    content: "this is a post, and I'm going to test it test test test can it be long so I cn truncate loooooong post here",
-    user_id: "1",
-    image_urls: null,
-    seller_name: "Meagan Seller",
-    seller_id: "6", 
-    email: "me@me.com",
-    price_id: "50",
-    product_id: "4",
-    product_subject: "4",
-    post_grade: "5",
-    resource_types: "2",
-    listing_status: true,
-
-    //These fields are not stored in the database and must be fetched from stripe (price) or set by the code
-    subject: null,
-    grade: null,//Array of grade names
-    resourceTypes: null, //Array of resourceTypes names
-    image_url: undefined,
-    seller_img: undefined,
-    price: 97, //Price of the post
-    quantity: 1 //Quantity to add to cart from post
-}
-
 export const CheckoutStatus: Component = () => {
     // TODO: Merge anonymous user to a real user with stripe email
     const [session, setSession] = createSignal<Stripe.Checkout.Session>();
@@ -48,7 +22,7 @@ export const CheckoutStatus: Component = () => {
     const [paid, setPaid] = createSignal(false);
 
     onMount(async () => {
-        // await CurrentCheckoutStatus(); // TODO: comment back in before committing
+        await CurrentCheckoutStatus();
     });
 
     function returnToResources() {
@@ -56,7 +30,7 @@ export const CheckoutStatus: Component = () => {
     }
 
     function viewMyPurchases() {
-        window.location.href = `/${ lang }/user/profile`;
+        window.location.href = `/${lang}/user/profile`;
     }
 
     async function CurrentCheckoutStatus() {
@@ -127,7 +101,7 @@ export const CheckoutStatus: Component = () => {
             .from("order_details")
             .select("*")
             // .eq("order_number", session()?.metadata?.orderId); //TODO: comment back in before committing
-            .eq("order_number", ("84dfd0fd-de56-4d7e-b402-d9aa0fe1d771")) //TODO: remove before committing
+            .eq("order_number", "84dfd0fd-de56-4d7e-b402-d9aa0fe1d771"); //TODO: remove before committing
         if (error) {
             console.log(
                 "Order Details Error: " + error.code + " " + error.message
@@ -155,14 +129,14 @@ export const CheckoutStatus: Component = () => {
     return (
         <div class="">
             <div
-                class="flex-col items-center justify-center bg-btn1 text-white py-2 md:py-6" //TODO: add "hidden" before committing
+                class="hidden flex-col items-center justify-center bg-btn1 py-2 text-white md:py-6"
                 id="success"
             >
                 <div class="flex justify-center">
                     <svg
                         viewBox="0 0 24 24"
                         id="check-mark-circle"
-                        class="h-16 w-16 md:h-36 md:w-36 text-btn1"
+                        class="h-16 w-16 text-btn1 md:h-36 md:w-36"
                     >
                         <circle cx="12" cy="12" r="10" fill="white"></circle>
                         <path
@@ -171,70 +145,91 @@ export const CheckoutStatus: Component = () => {
                         ></path>
                     </svg>
                 </div>
-                <p class="text-xl md:text-3xl font-bold text-center md:mt-2">{t("checkout.success")}</p>
-                <p class="italic text-center md:my-2">{t("checkout.thankYou")}</p>
+                <p class="text-center text-xl font-bold md:mt-2 md:text-3xl">
+                    {t("checkout.success")}
+                </p>
+                <p class="text-center italic md:my-2">
+                    {t("checkout.thankYou")}
+                </p>
             </div>
-            <div class="flex justify-center w-full">
-                <div class="w-96 md:w-full md:max-w-[500px] mt-4">
-                    <div class="text-lg">{t("checkout.orderID")}: {session()?.metadata?.orderId}</div>
-                    <div class="flex justify-between mb-4 mt-2">
+            <div class="flex w-full justify-center">
+                <div class="mt-4 w-96 md:w-full md:max-w-[500px]">
+                    <div class="text-lg">
+                        {t("checkout.orderID")}: {session()?.metadata?.orderId}
+                    </div>
+                    <div class="mb-4 mt-2 flex justify-between">
                         <p class="font-bold">{t("checkout.total")}</p>
-                        
-                        <p class="font-bold">${session()?.amount_total
-                            ? (session()!.amount_total! / 100).toFixed(2)
-                            : 0}
+
+                        <p class="font-bold">
+                            $
+                            {session()?.amount_total
+                                ? (session()!.amount_total! / 100).toFixed(2)
+                                : 0}
                         </p>
                     </div>
                     <div>
                         <p class="font-bold">{t("checkout.purchases")}</p>
                         {/* TODO: Clean up rendering */}
-                        {products().map((item) => (
+                        {/* {products().map((item) => (
                             <div class="grid grid-cols-2">
                                 <p>{item.title}</p>
-                                {/* TODO: Fix to actually allow download */}
-                                <button disabled={!paid()}>Download Button</button>
+                                TODO: Fix to actually allow download
+                                <button disabled={!paid()}>
+                                    Download Button
+                                </button>
                             </div>
-                        ))}
+                        ))} */}
 
-                        {/* TODO: apply this styling to the products().map */}
-                        <div class="flex justify-between my-2 md:my-4">
-                            <p class="line-clamp-2 w-2/3">This is a really long resource name so that I can test the truncate styling - but no, make it even longer...LONGER!!!</p>
-
-                            <button class="bg-downloadBtn rounded-full">
-                                <svg width="60px" height="40px" viewBox="0 0 24 24" fill="none" class="fill-white py-1">
-                                    <path d="M18.22 20.75H5.78C5.43322 20.7359 5.09262 20.6535 4.77771 20.5075C4.4628 20.3616 4.17975 20.155 3.94476 19.8996C3.70977 19.6442 3.52745 19.3449 3.40824 19.019C3.28903 18.693 3.23525 18.3468 3.25 18V15C3.25 14.8011 3.32902 14.6103 3.46967 14.4697C3.61033 14.329 3.80109 14.25 4 14.25C4.19892 14.25 4.38968 14.329 4.53033 14.4697C4.67099 14.6103 4.75 14.8011 4.75 15V18C4.72419 18.2969 4.81365 18.5924 4.99984 18.8251C5.18602 19.0579 5.45465 19.21 5.75 19.25H18.22C18.5154 19.21 18.784 19.0579 18.9702 18.8251C19.1564 18.5924 19.2458 18.2969 19.22 18V15C19.22 14.8011 19.299 14.6103 19.4397 14.4697C19.5803 14.329 19.7711 14.25 19.97 14.25C20.1689 14.25 20.3597 14.329 20.5003 14.4697C20.641 14.6103 20.72 14.8011 20.72 15V18C20.75 18.6954 20.5041 19.3744 20.0359 19.8894C19.5677 20.4045 18.9151 20.7137 18.22 20.75Z" fill="none" class="fill-white"/>
-                                    <path d="M12 15.75C11.9015 15.7504 11.8038 15.7312 11.7128 15.6934C11.6218 15.6557 11.5392 15.6001 11.47 15.53L7.47 11.53C7.33752 11.3878 7.2654 11.1997 7.26882 11.0054C7.27225 10.8111 7.35096 10.6258 7.48838 10.4883C7.62579 10.3509 7.81118 10.2722 8.00548 10.2688C8.19978 10.2654 8.38782 10.3375 8.53 10.47L12 13.94L15.47 10.47C15.6122 10.3375 15.8002 10.2654 15.9945 10.2688C16.1888 10.2722 16.3742 10.3509 16.5116 10.4883C16.649 10.6258 16.7277 10.8111 16.7312 11.0054C16.7346 11.1997 16.6625 11.3878 16.53 11.53L12.53 15.53C12.4608 15.6001 12.3782 15.6557 12.2872 15.6934C12.1962 15.7312 12.0985 15.7504 12 15.75Z" fill="none" class="fill-white"/>
-                                    <path d="M12 15.75C11.8019 15.7474 11.6126 15.6676 11.4725 15.5275C11.3324 15.3874 11.2526 15.1981 11.25 15V4C11.25 3.80109 11.329 3.61032 11.4697 3.46967C11.6103 3.32902 11.8011 3.25 12 3.25C12.1989 3.25 12.3897 3.32902 12.5303 3.46967C12.671 3.61032 12.75 3.80109 12.75 4V15C12.7474 15.1981 12.6676 15.3874 12.5275 15.5275C12.3874 15.6676 12.1981 15.7474 12 15.75Z" fill="none" class="fill-white"/>
-                                </svg>
-                            </button>
-                        </div>
-                            
                         {products().map((item) => (
-                            <div class="flex justify-between my-2 md:my-4">
-                            <a href={`/${lang}/posts/${ item.id }`}><p class="line-clamp-2 w-2/3">This is a really long resource name so that I can test the truncate styling - but no, make it even longer...LONGER!!!</p></a>
+                            <div class="my-2 flex justify-between md:my-4">
+                                <a href={`/${lang}/posts/${item.id}`}>
+                                    <p class="line-clamp-2 w-2/3">
+                                        This is a really long resource name so
+                                        that I can test the truncate styling -
+                                        but no, make it even longer...LONGER!!!
+                                    </p>
+                                </a>
 
-                            <button class="bg-downloadBtn rounded-full">
-                                <svg width="60px" height="40px" viewBox="0 0 24 24" fill="none" class="fill-white py-1">
-                                    <path d="M18.22 20.75H5.78C5.43322 20.7359 5.09262 20.6535 4.77771 20.5075C4.4628 20.3616 4.17975 20.155 3.94476 19.8996C3.70977 19.6442 3.52745 19.3449 3.40824 19.019C3.28903 18.693 3.23525 18.3468 3.25 18V15C3.25 14.8011 3.32902 14.6103 3.46967 14.4697C3.61033 14.329 3.80109 14.25 4 14.25C4.19892 14.25 4.38968 14.329 4.53033 14.4697C4.67099 14.6103 4.75 14.8011 4.75 15V18C4.72419 18.2969 4.81365 18.5924 4.99984 18.8251C5.18602 19.0579 5.45465 19.21 5.75 19.25H18.22C18.5154 19.21 18.784 19.0579 18.9702 18.8251C19.1564 18.5924 19.2458 18.2969 19.22 18V15C19.22 14.8011 19.299 14.6103 19.4397 14.4697C19.5803 14.329 19.7711 14.25 19.97 14.25C20.1689 14.25 20.3597 14.329 20.5003 14.4697C20.641 14.6103 20.72 14.8011 20.72 15V18C20.75 18.6954 20.5041 19.3744 20.0359 19.8894C19.5677 20.4045 18.9151 20.7137 18.22 20.75Z" fill="none" class="fill-white"/>
-                                    <path d="M12 15.75C11.9015 15.7504 11.8038 15.7312 11.7128 15.6934C11.6218 15.6557 11.5392 15.6001 11.47 15.53L7.47 11.53C7.33752 11.3878 7.2654 11.1997 7.26882 11.0054C7.27225 10.8111 7.35096 10.6258 7.48838 10.4883C7.62579 10.3509 7.81118 10.2722 8.00548 10.2688C8.19978 10.2654 8.38782 10.3375 8.53 10.47L12 13.94L15.47 10.47C15.6122 10.3375 15.8002 10.2654 15.9945 10.2688C16.1888 10.2722 16.3742 10.3509 16.5116 10.4883C16.649 10.6258 16.7277 10.8111 16.7312 11.0054C16.7346 11.1997 16.6625 11.3878 16.53 11.53L12.53 15.53C12.4608 15.6001 12.3782 15.6557 12.2872 15.6934C12.1962 15.7312 12.0985 15.7504 12 15.75Z" fill="none" class="fill-white"/>
-                                    <path d="M12 15.75C11.8019 15.7474 11.6126 15.6676 11.4725 15.5275C11.3324 15.3874 11.2526 15.1981 11.25 15V4C11.25 3.80109 11.329 3.61032 11.4697 3.46967C11.6103 3.32902 11.8011 3.25 12 3.25C12.1989 3.25 12.3897 3.32902 12.5303 3.46967C12.671 3.61032 12.75 3.80109 12.75 4V15C12.7474 15.1981 12.6676 15.3874 12.5275 15.5275C12.3874 15.6676 12.1981 15.7474 12 15.75Z" fill="none" class="fill-white"/>
-                                </svg>
-                            </button>
-                        </div>
+                                <button class="bg-downloadBtn rounded-full">
+                                    <svg
+                                        width="60px"
+                                        height="40px"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        class="fill-white py-1"
+                                    >
+                                        <path
+                                            d="M18.22 20.75H5.78C5.43322 20.7359 5.09262 20.6535 4.77771 20.5075C4.4628 20.3616 4.17975 20.155 3.94476 19.8996C3.70977 19.6442 3.52745 19.3449 3.40824 19.019C3.28903 18.693 3.23525 18.3468 3.25 18V15C3.25 14.8011 3.32902 14.6103 3.46967 14.4697C3.61033 14.329 3.80109 14.25 4 14.25C4.19892 14.25 4.38968 14.329 4.53033 14.4697C4.67099 14.6103 4.75 14.8011 4.75 15V18C4.72419 18.2969 4.81365 18.5924 4.99984 18.8251C5.18602 19.0579 5.45465 19.21 5.75 19.25H18.22C18.5154 19.21 18.784 19.0579 18.9702 18.8251C19.1564 18.5924 19.2458 18.2969 19.22 18V15C19.22 14.8011 19.299 14.6103 19.4397 14.4697C19.5803 14.329 19.7711 14.25 19.97 14.25C20.1689 14.25 20.3597 14.329 20.5003 14.4697C20.641 14.6103 20.72 14.8011 20.72 15V18C20.75 18.6954 20.5041 19.3744 20.0359 19.8894C19.5677 20.4045 18.9151 20.7137 18.22 20.75Z"
+                                            fill="none"
+                                            class="fill-white"
+                                        />
+                                        <path
+                                            d="M12 15.75C11.9015 15.7504 11.8038 15.7312 11.7128 15.6934C11.6218 15.6557 11.5392 15.6001 11.47 15.53L7.47 11.53C7.33752 11.3878 7.2654 11.1997 7.26882 11.0054C7.27225 10.8111 7.35096 10.6258 7.48838 10.4883C7.62579 10.3509 7.81118 10.2722 8.00548 10.2688C8.19978 10.2654 8.38782 10.3375 8.53 10.47L12 13.94L15.47 10.47C15.6122 10.3375 15.8002 10.2654 15.9945 10.2688C16.1888 10.2722 16.3742 10.3509 16.5116 10.4883C16.649 10.6258 16.7277 10.8111 16.7312 11.0054C16.7346 11.1997 16.6625 11.3878 16.53 11.53L12.53 15.53C12.4608 15.6001 12.3782 15.6557 12.2872 15.6934C12.1962 15.7312 12.0985 15.7504 12 15.75Z"
+                                            fill="none"
+                                            class="fill-white"
+                                        />
+                                        <path
+                                            d="M12 15.75C11.8019 15.7474 11.6126 15.6676 11.4725 15.5275C11.3324 15.3874 11.2526 15.1981 11.25 15V4C11.25 3.80109 11.329 3.61032 11.4697 3.46967C11.6103 3.32902 11.8011 3.25 12 3.25C12.1989 3.25 12.3897 3.32902 12.5303 3.46967C12.671 3.61032 12.75 3.80109 12.75 4V15C12.7474 15.1981 12.6676 15.3874 12.5275 15.5275C12.3874 15.6676 12.1981 15.7474 12 15.75Z"
+                                            fill="none"
+                                            class="fill-white"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
                         ))}
                     </div>
 
-                    <div class="flex flex-col md:flex-row items-center mt-8 md:mt-24 md:mb-8 justify-center">
-                        <button 
-                            class="border border-border1 dark:border-border1-DM w-64 my-1 rounded py-1 md:mx-4"
-                            onClick={ viewMyPurchases }
+                    <div class="mt-8 flex flex-col items-center justify-center md:mb-8 md:mt-24 md:flex-row">
+                        <button
+                            class="my-1 w-64 rounded border border-border1 py-1 dark:border-border1-DM md:mx-4"
+                            onClick={viewMyPurchases}
                         >
                             {t("buttons.viewOrders")}
                         </button>
 
-                        <button 
-                            class="bg-btn1 dark:bg-btn1-DM, rounded w-64 text-ptext2 dark:text-ptext2-DM my-1 py-1 md:mx-4"
-                            onClick={ returnToResources }
+                        <button
+                            class="dark:bg-btn1-DM, my-1 w-64 rounded bg-btn1 py-1 text-ptext2 dark:text-ptext2-DM md:mx-4"
+                            onClick={returnToResources}
                         >
                             {t("buttons.continueShopping")}
                         </button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,108 +1,108 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				//Dark Mode
-				'background1-DM': '#000000',
-				'htext1-DM': '#1DD762',
-				'ptext1-DM': '#F0F0F0',
-				'btn1-DM':	'#1DD762',
-				'btn1Text-DM': '#000000',
-				'link1-DM': '#1DD762',
-				'link1Hov-DM': '#17a84d',
-				'alert1-DM': '#ff4e4e',
-				'border1-DM': '#F0F0F0',
-				'icon1-DM': '#F0F0F0',
-				'logo1-DM': '#F0F0F0',
-				'highlight1-DM': '#1DD762',
-				'iconbg1-DM': '#FFFFFF',
-				'inputBorder1-DM': '#A9A9A9',
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                //Dark Mode
+                "background1-DM": "#000000",
+                "htext1-DM": "#1DD762",
+                "ptext1-DM": "#F0F0F0",
+                "btn1-DM": "#1DD762",
+                "btn1Text-DM": "#000000",
+                "link1-DM": "#1DD762",
+                "link1Hov-DM": "#17a84d",
+                "alert1-DM": "#ff4e4e",
+                "border1-DM": "#F0F0F0",
+                "icon1-DM": "#F0F0F0",
+                "logo1-DM": "#F0F0F0",
+                "highlight1-DM": "#1DD762",
+                "iconbg1-DM": "#FFFFFF",
+                "inputBorder1-DM": "#A9A9A9",
 
-				'background2-DM': '#1F2937',
-				'htext2-DM': '#1DD762',
-				'ptext2-DM': '#F0F0F0',
-				'btn2-DM': '#1DD762',
-				'btn2Text-DM': '#000000',
-				'link2-DM': '#1DD762',
-				'link2Hov-DM': '#17a84d',
-				'alert2-DM': '#ff4e4e',
-				'border2-DM': '#F0F0F0',
-				'icon2-DM': '#F0F0F0',
-				'logo2-DM': '#F0F0F0',
-				'highlight2-DM': '#1DD762',
-				'iconbg2-DM': '',
-				'inputBorder2-DM': '',
+                "background2-DM": "#1F2937",
+                "htext2-DM": "#1DD762",
+                "ptext2-DM": "#F0F0F0",
+                "btn2-DM": "#1DD762",
+                "btn2Text-DM": "#000000",
+                "link2-DM": "#1DD762",
+                "link2Hov-DM": "#17a84d",
+                "alert2-DM": "#ff4e4e",
+                "border2-DM": "#F0F0F0",
+                "icon2-DM": "#F0F0F0",
+                "logo2-DM": "#F0F0F0",
+                "highlight2-DM": "#1DD762",
+                "iconbg2-DM": "",
+                "inputBorder2-DM": "",
 
-				//Light Mode
-				'background1': '#FFFFFF',
-				'htext1': '#198900',
-				'ptext1': '#000000',
-				'btn1': '#198900',
-				'btn1Text': '#FFFFFF',
-				'link1': '#198900',
-				'link1Hov': '#005211',
-				'alert1': '#c20000',
-				'border1': '#000000',
-				'icon1': '#000000',
-				'logo1': '#000000',
-				'highlight1': '#198900',
-				'iconbg1': '#FFFFFF',
-				'inputBorder1': '#A9A9A9',
+                //Light Mode
+                background1: "#FFFFFF",
+                htext1: "#198900",
+                ptext1: "#000000",
+                btn1: "#198900",
+                btn1Text: "#FFFFFF",
+                link1: "#198900",
+                link1Hov: "#005211",
+                alert1: "#c20000",
+                border1: "#000000",
+                icon1: "#000000",
+                logo1: "#000000",
+                highlight1: "#198900",
+                iconbg1: "#FFFFFF",
+                inputBorder1: "#A9A9A9",
 
-				'background2': '#2D2D2D',
-				'htext2': '#25DA49',
-				'ptext2': '#F0F0F0',
-				'btn2': '#25DA49',
-				'btn2Text': '#2D2D2D',
-				'link2': '#1DD762',
-				'link2Hov': '#22a951',
-				'alert2': '#fb5e5e',
-				'border2': '#F0F0F0',
-				'icon2': '#F0F0F0',
-				'logo2': '#F0F0F0',
-				'highlight2': '#25DA49',
-				'iconbg2': '',
-				'inputBorder2': '',
+                background2: "#2D2D2D",
+                htext2: "#25DA49",
+                ptext2: "#F0F0F0",
+                btn2: "#25DA49",
+                btn2Text: "#2D2D2D",
+                link2: "#1DD762",
+                link2Hov: "#22a951",
+                alert2: "#fb5e5e",
+                border2: "#F0F0F0",
+                icon2: "#F0F0F0",
+                logo2: "#F0F0F0",
+                highlight2: "#25DA49",
+                iconbg2: "",
+                inputBorder2: "",
 
-				'shadow-LM': '#9E9E9E',
-				'shadow-DM': '#616161',
+                "shadow-LM": "#9E9E9E",
+                "shadow-DM": "#616161",
 
-				'downloadBtn': '#030089',
-			},
-			typography: {
-				DEFAULT: {
-					css: {
-						blockquote: {
-							fontStyle: 'normal',
-						},
-						'blockquote p:first-of-type::before': {
-							content: 'none',
-						},
-						'blockquote p:last-of-type::after': {
-							content: 'none',
-						},
-					}
-				}
-			},
-			keyframes: {
-				click: {
-				  "0%, 100%": { transform: "translateY(0px)" },
-				  "50%": { transform: "translateY(0.125rem)" }
-				}
-			  },
-			  animation: {
-				click: "click 200ms ease-in-out"
-			  },
-			inset: {
-				'2': '2px',
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('tailwind-scrollbar')({ nocompatible: true }),
-	]
-}
+                downloadBtn: "#030089",
+            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        blockquote: {
+                            fontStyle: "normal",
+                        },
+                        "blockquote p:first-of-type::before": {
+                            content: "none",
+                        },
+                        "blockquote p:last-of-type::after": {
+                            content: "none",
+                        },
+                    },
+                },
+            },
+            keyframes: {
+                click: {
+                    "0%, 100%": { transform: "translateY(0px)" },
+                    "50%": { transform: "translateY(0.125rem)" },
+                },
+            },
+            animation: {
+                click: "click 200ms ease-in-out",
+            },
+            inset: {
+                2: "2px",
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("tailwind-scrollbar")({ nocompatible: true }),
+    ],
+};
