81279f73de04f5b11891e3ff08ec2beb632eb599
r-southworth <rachael.southworth@datagrove.com>
2024-07-19 15:42:18 -0400

    update banner

--- src/components/common/notices/Banner.tsx
+++ src/components/common/notices/Banner.tsx
@@ -3,18 +3,25 @@ import { createSignal, createEffect, onCleanup, Show } from "solid-js";
 
 type BannerProps = {
     content: string | JSX.Element;
-    linkLocation: string;
-    linkLabel: string;
+    linkLocation?: string;
+    linkLabel?: string;
 };
 
 const Banner: Component<BannerProps> = (props) => {
     return (
         <>
+        <Show when={props.linkLocation}>
         <a href={props.linkLocation} aria-label={props.linkLabel} target="_blank">
             <div class="w-full bg-btn1-DM text-center">
                 <div class="max-w-[calc(100vw-4rem)] mx-auto prose line-clamp-2">{props.content}</div>
             </div>
         </a>
+        </Show>
+        <Show when={!props.linkLocation}>
+            <div class="w-full bg-btn1-DM text-center">
+                <div class="max-w-[calc(100vw-4rem)] mx-auto prose line-clamp-2">{props.content}</div>
+            </div>
+        </Show>
         </>
     );
 };
