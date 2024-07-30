7f2b3e60ebbb0cda0820b7a7a92cb9854e1eac08
Meagan <meagan.h.smith@gmail.com>
2024-07-30 13:41:10 -0500

    Apply prettier formatting

--- src/components/posts/AddFavorite.tsx
+++ src/components/posts/AddFavorite.tsx
@@ -148,7 +148,7 @@ export const FavoriteButton: Component<Props> = (props) => {
                         stroke="none"
                         viewBox="0 0 512.00 512.00"
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-8 w-8 fill-icon1 dark:fill-icon1-DM rounded-full border border-border2 dark:border-border1"
+                        class="h-8 w-8 rounded-full border border-border2 fill-icon1 dark:border-border1 dark:fill-icon1-DM"
                     >
                         <g
                             id="SVGRepo_bgCarrier"
@@ -171,7 +171,10 @@ export const FavoriteButton: Component<Props> = (props) => {
                             stroke-linejoin="round"
                         ></g>
                         <g id="SVGRepo_iconCarrier">
-                            <path class="" d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48Zm74.69,252.82c-9.38,11.44-26.4,29.73-65.7,56.41a15.93,15.93,0,0,1-18,0c-39.3-26.68-56.32-45-65.7-56.41-20-24.37-29.58-49.4-29.3-76.5.31-31.06,25.22-56.33,55.53-56.33,20.4,0,35,10.63,44.1,20.41a6,6,0,0,0,8.72,0c9.11-9.78,23.7-20.41,44.1-20.41,30.31,0,55.22,25.27,55.53,56.33C360.27,251.42,350.68,276.45,330.69,300.82Z"></path>
+                            <path
+                                class=""
+                                d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48Zm74.69,252.82c-9.38,11.44-26.4,29.73-65.7,56.41a15.93,15.93,0,0,1-18,0c-39.3-26.68-56.32-45-65.7-56.41-20-24.37-29.58-49.4-29.3-76.5.31-31.06,25.22-56.33,55.53-56.33,20.4,0,35,10.63,44.1,20.41a6,6,0,0,0,8.72,0c9.11-9.78,23.7-20.41,44.1-20.41,30.31,0,55.22,25.27,55.53,56.33C360.27,251.42,350.68,276.45,330.69,300.82Z"
+                            ></path>
                         </g>
                     </svg>
                 </button>
@@ -186,7 +189,7 @@ export const FavoriteButton: Component<Props> = (props) => {
                         stroke="none"
                         viewBox="0 0 512.00 512.00"
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-8 w-8 fill-icon1 dark:fill-icon1-DM rounded-full border border-inputBorder1 dark:border-inputBorder1-DM bg-icon1 dark:bg-icon1-DM"
+                        class="h-8 w-8 rounded-full border border-inputBorder1 bg-icon1 fill-icon1 dark:border-inputBorder1-DM dark:bg-icon1-DM dark:fill-icon1-DM"
                     >
                         <g
                             id="SVGRepo_bgCarrier"
